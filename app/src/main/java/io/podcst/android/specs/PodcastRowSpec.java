ad5ee040c94fdb7a90d513e010c7daf69825aac4
Shantanu Raj <s@sraj.me>
2017-04-19 22:52:57 +0530

    Launch DetailsActvity on click

--- app/src/main/java/io/podcst/android/specs/PodcastRowSpec.java
+++ app/src/main/java/io/podcst/android/specs/PodcastRowSpec.java
@@ -1,22 +1,30 @@
 package io.podcst.android.specs;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.litho.ClickEvent;
 import com.facebook.litho.Column;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.ComponentLayout;
 import com.facebook.litho.Row;
+import com.facebook.litho.annotations.FromEvent;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.annotations.OnEvent;
 import com.facebook.litho.annotations.Prop;
 import com.facebook.litho.fresco.FrescoImage;
 import com.facebook.litho.widget.Text;
 import com.facebook.yoga.YogaEdge;
 
 import io.podcst.android.R;
+import io.podcst.android.data.Constants;
 import io.podcst.android.data.Podcast;
+import io.podcst.android.ui.DetailsActivity;
 
 /**
  * Created by eve on 19/04/17.
@@ -46,6 +54,7 @@ public class PodcastRowSpec {
 
         return Row.create(c)
                 .backgroundColor(Color.WHITE)
+                .clickHandler(PodcastRow.onClick(c))
                 .child(
                         FrescoImage.create(c)
                                 .placeholderImageRes(R.drawable.ic_placeholder_cover)
@@ -56,5 +65,16 @@ public class PodcastRowSpec {
                 ).child(
                         text
                 ).build();
+    }
+
+    @OnEvent(ClickEvent.class)
+    static void onClick(
+            ComponentContext c,
+            @FromEvent View view,
+            @Prop Podcast podcast
+    ) {
+        Intent intent = new Intent(c.getApplicationContext(), DetailsActivity.class);
+        intent.putExtra(Constants.DETAILS_BUNDLE, podcast);
+        c.startActivity(intent);
     }
 }
