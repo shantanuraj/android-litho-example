446c13f9159cb0f5a56590213a38b39bf1404bc4
Shantanu Raj <s@sraj.me>
2017-04-19 23:23:50 +0530

    Display podcast description

--- app/src/main/java/io/podcst/android/specs/PodcastDetailsSpec.java
+++ app/src/main/java/io/podcst/android/specs/PodcastDetailsSpec.java
@@ -1,6 +1,10 @@
 package io.podcst.android.specs;
 
 import android.graphics.Color;
+import android.os.Build;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.SpannedString;
 import android.view.ViewGroup;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
@@ -29,9 +33,16 @@ public class PodcastDetailsSpec {
     @OnCreateLayout
     static ComponentLayout onCreateLayout(ComponentContext c, @Prop Podcast podcast) {
 
-        DraweeController controller = Fresco.newDraweeControllerBuilder()
+        final DraweeController controller = Fresco.newDraweeControllerBuilder()
                 .setUri(podcast.cover)
                 .build();
+
+        Spanned desc;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            desc = Html.fromHtml(podcast.description, Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            desc = Html.fromHtml(podcast.description);
+        }
 
         ComponentLayout.ContainerBuilder text = Column.create(c)
                 .paddingDip(YogaEdge.ALL, 16)
@@ -44,6 +55,10 @@ public class PodcastDetailsSpec {
                         Text.create(c)
                                 .text(podcast.author)
                                 .textSizeSp(12)
+                ).child(
+                        Text.create(c)
+                                .text(desc)
+                                .textSizeSp(16)
                 );
 
         return Column.create(c)
