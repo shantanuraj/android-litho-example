db47ff19789f87a8ddc7aff7da9c7de631602ae6
Shantanu Raj <s@sraj.me>
2017-04-20 00:39:11 +0530

    Hacky scroll

--- app/src/main/java/io/podcst/android/ui/DetailsActivity.java
+++ app/src/main/java/io/podcst/android/ui/DetailsActivity.java
@@ -3,10 +3,14 @@ package io.podcst.android.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.OrientationHelper;
 
 import com.facebook.litho.Component;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.LithoView;
+import com.facebook.litho.widget.LinearLayoutInfo;
+import com.facebook.litho.widget.Recycler;
+import com.facebook.litho.widget.RecyclerBinder;
 
 import io.podcst.android.data.Constants;
 import io.podcst.android.data.Podcast;
@@ -33,9 +37,22 @@ public class DetailsActivity extends AppCompatActivity {
 
         final Podcast podcast = bundle.getParcelable(Constants.DETAILS_BUNDLE);
 
-        final Component view = PodcastDetails.create(viewContext)
-                .podcast(podcast)
+        final RecyclerBinder recyclerBinder = new RecyclerBinder(
+                viewContext,
+                new LinearLayoutInfo(
+                        getApplicationContext(),
+                        OrientationHelper.VERTICAL,
+                        false
+                ));
+
+        final Component view = Recycler.create(viewContext)
+                .binder(recyclerBinder)
                 .build();
+
+        recyclerBinder.insertItemAt(0, PodcastDetails
+                .create(viewContext)
+                .podcast(podcast)
+                .build());
 
         final LithoView lithoView = LithoView.create(
                 this,
