c7f86def6158be694ff95f2028d3e87912abb430
Shantanu Raj <s@sraj.me>
2017-04-21 00:05:23 +0530

    Add support for multiple fragments

---
+++ app/src/main/java/io/podcst/android/ui/MainFragment.java
@@ -0,0 +1,81 @@
+package io.podcst.android.ui;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import io.podcst.android.data.Podcast;
+
+public class MainFragment extends Fragment {
+
+    private static final String PODCASTS_CATEGORY = "podcastsCategory";
+
+    private String podcastsCategory;
+    private List<Podcast> podcasts;
+
+    private OnFragmentInteractionListener mListener;
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainFragment newInstance(String podcastsCategory) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putString(PODCASTS_CATEGORY, podcastsCategory);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            podcastsCategory = getArguments().getString(PODCASTS_CATEGORY);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(podcastsCategory);
+        return textView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/io/podcst/android/ui/NuMainActivity.java
+++ app/src/main/java/io/podcst/android/ui/NuMainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import io.podcst.android.R;
+import io.podcst.android.data.Constants;
 
 /**
  * Created by eve on 19/04/17.
@@ -20,15 +21,39 @@ public class NuMainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            String podcastsCategory = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_popular:
+                    podcastsCategory = Constants.POPULAR; break;
+                case R.id.navigation_featured:
+                    podcastsCategory = Constants.FEATURED; break;
+                case R.id.navigation_trending:
+                    podcastsCategory = Constants.TRENDING; break;
+            }
+            if (podcastsCategory != null) {
+                updateFragment(podcastsCategory);
+            }
             return true;
         }
 
     };
+
+    private void updateFragment(final String podcastsCategory) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, MainFragment.newInstance(podcastsCategory))
+                .commit();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        updateFragment(Constants.FEATURED);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
