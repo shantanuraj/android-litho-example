544e4be2c1a53501d2517a659f62260daf867de3
Shantanu Raj <s@sraj.me>
2017-04-21 00:53:05 +0530

    Add Loading component

---
+++ app/src/main/java/io/podcst/android/specs/LoadingSpec.java
@@ -0,0 +1,33 @@
+package io.podcst.android.specs;
+
+import com.facebook.litho.Column;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.ComponentLayout;
+import com.facebook.litho.annotations.LayoutSpec;
+import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.widget.Text;
+import com.facebook.yoga.YogaAlign;
+import com.facebook.yoga.YogaJustify;
+
+import io.podcst.android.R;
+
+/**
+ * Created by eve on 19/04/17.
+ */
+
+@LayoutSpec
+public class LoadingSpec {
+
+    @OnCreateLayout
+    static ComponentLayout onCreateLayout(ComponentContext c) {
+
+        return Column.create(c)
+                .alignContent(YogaAlign.CENTER)
+                .justifyContent(YogaJustify.CENTER)
+                .child(
+                        Text.create(c)
+                                .textRes(R.string.loading)
+                                .textSizeSp(16)
+                ).build();
+    }
+}
