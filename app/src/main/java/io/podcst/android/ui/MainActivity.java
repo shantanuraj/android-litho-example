da23848f21a3f90b6be234986331797dfbda0e4b
Shantanu Raj <s@sraj.me>
2017-04-21 00:23:22 +0530

    Rename Activity

--- app/src/main/java/io/podcst/android/ui/MainActivity.java
+++ app/src/main/java/io/podcst/android/ui/MainActivity.java
@@ -1,29 +1,14 @@
 package io.podcst.android.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.OrientationHelper;
-import android.widget.Toast;
+import android.view.MenuItem;
 
-import com.facebook.litho.Component;
-import com.facebook.litho.ComponentContext;
-import com.facebook.litho.ComponentInfo;
-import com.facebook.litho.LithoView;
-import com.facebook.litho.widget.LinearLayoutInfo;
-import com.facebook.litho.widget.Recycler;
-import com.facebook.litho.widget.RecyclerBinder;
-
-import java.util.List;
-
-import io.podcst.android.data.Api;
+import io.podcst.android.R;
 import io.podcst.android.data.Constants;
-import io.podcst.android.data.Podcast;
-import io.podcst.android.data.PodcastsResponse;
-import io.podcst.android.specs.PodcastRowCard;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by eve on 19/04/17.
@@ -31,75 +16,44 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Api.ApiService api;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-    private RecyclerBinder recyclerBinder;
-    private ComponentContext viewContext;
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            String podcastsCategory = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_popular:
+                    podcastsCategory = Constants.POPULAR; break;
+                case R.id.navigation_featured:
+                    podcastsCategory = Constants.FEATURED; break;
+                case R.id.navigation_trending:
+                    podcastsCategory = Constants.TRENDING; break;
+            }
+            if (podcastsCategory != null) {
+                updateFragment(podcastsCategory);
+            }
+            return true;
+        }
+
+    };
+
+    private void updateFragment(final String podcastsCategory) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, MainFragment.newInstance(podcastsCategory))
+                .commit();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        api = Api.get();
+        setContentView(R.layout.activity_main);
 
-        viewContext = new ComponentContext(this);
+        updateFragment(Constants.FEATURED);
 
-        recyclerBinder = new RecyclerBinder(
-                viewContext,
-                new LinearLayoutInfo(
-                        getApplicationContext(),
-                        OrientationHelper.VERTICAL,
-                        false
-                ));
-
-        final Component component = Recycler.create(viewContext)
-                .binder(recyclerBinder)
-                .build();
-
-        final LithoView lithoView = LithoView.create(
-                this,
-                component);
-
-        setContentView(lithoView);
-
-        loadPodcasts();
-    }
-
-    private static void populateList(RecyclerBinder recyclerBinder,
-                                     ComponentContext c,
-                                     List<Podcast> podcasts) {
-        int i = 0;
-        for (Podcast podcast : podcasts) {
-            ComponentInfo.Builder componentInfoBuilder = ComponentInfo.create();
-            componentInfoBuilder.component(
-                    PodcastRowCard.create(c)
-                        .podcast(podcast)
-                        .build());
-
-            recyclerBinder.insertItemAt(i++,  componentInfoBuilder.build());
-        }
-    }
-
-    private void loadPodcasts() {
-        api.getPodcasts(Constants.FEATURED)
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DisposableObserver<PodcastsResponse>() {
-                    @Override
-                    public void onNext(PodcastsResponse value) {
-                        populateList(recyclerBinder, viewContext, value.data);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Toast.makeText(getApplicationContext(),
-                                "Could not load podcasts",
-                                Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onComplete() {
-                    }
-                });
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
--- app/src/main/java/io/podcst/android/ui/MainFragment.java
+++ app/src/main/java/io/podcst/android/ui/MainFragment.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.OrientationHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.facebook.litho.Component;
